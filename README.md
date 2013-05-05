da324bd67f02de2ccd1251e8fc3d1d7df60ce5f4
RobFos <robert_foskin@hotmail.com>
2013-05-05 21:55:20 +0100

    final one

---
+++ README.md
@@ -0,0 +1,5 @@
+meteor.js 
+
+chat system
+
+designed by Robert Foskin D10119833
--- richweb.js
+++ richweb.js
@@ -1,7 +1,7 @@
 Message = new Meteor.Collection('messages');
+Coluser = new Meteor.Collection('ids');
 
 if (Meteor.isClient) {
-
 
 Accounts.ui.config(
 {
@@ -10,24 +10,47 @@ Accounts.ui.config(
 
 Template.login.events = {
 	'click #button':function(){
-		var user=document.getElementById('username');
+		var user=Meteor.user().username;
 		var text=document.getElementById('messagebox');
 		if(user.value!=""){
 			var ts=Date.now()/1000;
-			Message.insert({ name:user.value,message:text.value,time:ts});
+			Message.insert({ name:user,message:text.value,time:ts});
 			console.log("You sent a message");
+			$('#messagebox').val('');
+			var div=$("#chat");
+				div.animate({scrollTop:
+					div.prop("scrollHeight")});
 		}
 	}
 };
 
+//get all users
+Template.users.allusers=function(){
+	return Meteor.users.find({});
+};
 
+//check if user logged in
+Template.login.checkLog=function(){
+	if(Meteor.user()!=null){
+		return true;
+	}else{
+		return false
+	}
+};
+
+//get all the messages
 Template.messages.messages = function(){
-	return Message.find({},{sort: {time:-1} } );
+	var div=$("#chat");
+				div.animate({scrollTop:
+					div.prop("scrollHeight")});
+	return Message.find({},{sort: {time:1}} );
 	};
 }
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+	
   });
 }
